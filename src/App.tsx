b6a490fb3ac51ba42998aa8a3f7aee1270087213
KingJeason <1503268691@qq.com>
2018-11-13 12:38:28 +0800

    blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
-
+// import Button from '@material-ui/core/Button';
+// import logo from './logo.svg';
+import Blog from './Blog'
+// console.log(blog, '12123')
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Blog/>
     );
   }
 }
