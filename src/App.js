8d199d54e225977df92c09c040aa8fbfeafa036f
KingJeason <1503268691@qq.com>
2018-12-28 01:06:53 +0800

    给路由加上auth限制

--- src/App.js
+++ src/App.js
@@ -3,21 +3,68 @@ import './App.css';
 import Layout from './layouts/BasicLayout';
 import Draft from './pages/draft/index'
 import { Provider } from 'react-redux'
+import Cookies from 'js-cookie'
+import { connect } from 'react-redux'
+
 import store from './store/reducer'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { withRouter } from "react-router";
+
 @withRouter
 class App extends Component {
+  state = {
+    user: null
+  }
+  checkToken () {
+    // if (this.props.location.pathname != '/login') {
+    //   if (!Cookies.get('JSESSIONID')) {
+    //     this.props.history.replace('/login')
+    //   }
+    // } else {
+    //   if (Cookies.get('JSESSIONID')) {
+    //     this.props.history.replace('/home')
+    //   }
+    // }
+  }
+  componentWillMount () {
+    this.setState({
+      user: store.getState().user
+    })
+  }
+  componentWillReceiveProps () {
+    // this.checkToken()
+  }
   render () {
+    const { user } = this.state
+    const AuthComponent = ({component: Component}, ...rest) => {
+      return (
+        <Route
+          { ...rest }
+          render={ props =>
+            user ? (
+              <Component { ...props } />
+            ) : (
+                <Redirect
+                  to={ {
+                    pathname: "/",
+                    state: { from: props.location }
+                  } }
+                />
+              )
+          }
+        />
+      )
+    }
     return (
       <Provider store={ store }>
         <Switch>
-          <Route path="/drafts" component={ Draft } />
+          <AuthComponent path="/drafts"
+            component={ Draft } />
           <Route path="/" component={ Layout } />
-        </Switch> 
+        </Switch>
       </Provider>
     );
+    }
   }
-}
 
-export default App;
+  export default App;
